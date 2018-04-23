d2a53204c0d4d1299c17072dd74423944162ab65
strider90arun <strider90arun@gmail.com>
2018-04-22 19:17:57 -0700

    Refactoring

--- src/main/java/com/arun/spark/SparkOperations.java
+++ src/main/java/com/arun/spark/SparkOperations.java
@@ -8,9 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class SparkOperations {
 
